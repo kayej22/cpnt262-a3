ce622b87ab15bbbae9230f74b46e8d9e7581dd73
Joel Kaye <kayej22@hotmail.com>
2021-03-10 13:11:17 -0700

    added more comments to server file

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+/* Static asset from public directory */
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response){
   console.log(response);
   response.send('<h1>Wazzzzzzzzzup?</h1>');
 });
-
+/* Custom 404 page */
 app.use(function(request, response) {
   response.status(400);
   response.render('404.jade', {title: '404 File not Found'});
 });
-
+/* Default Port */
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
